3ea530061dd21813310f40206736a2ef800a7a00
haibangdang <haibang.dang@gmail.com>
2022-07-31 11:15:25 +0700

    initial

---
+++ src/components/Template/index.jsx
@@ -0,0 +1,9 @@
+// import { MqttClient } from 'mqtt'
+import React from 'react'
+
+const Template = ({ templateTest })  => {
+  return ( 
+    <div>index {templateTest}</div>
+  )
+}
+export default Template;
